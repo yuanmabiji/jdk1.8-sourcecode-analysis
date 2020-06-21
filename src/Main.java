81bfc6b5928c39a6b81baa5f6eafe0d2ce6931c8
jinyue <983656956@qq.com>
2020-06-21 10:14:22 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.concurrent.Executors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Executors.newSingleThreadExecutor();
+    }
+}
